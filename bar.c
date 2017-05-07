4e042603eb5ccf45eaa1914e09221cceef3e1ed6
nogaems <nogaems@users.noreply.github.com>
2017-05-07 22:06:46 +0300

    Added 'bar.c'

---
+++ bar.c
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the status-bar distribution (https://github.com/nogaems/status-bar)
+ * Copyright (c) 2017 by nogaems <nomad@ag.ru>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
