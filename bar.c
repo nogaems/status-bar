812d76affd0de718f8214e22276adebfa4ad5aaf
nogaems <nogaems@users.noreply.github.com>
2017-05-08 20:51:17 +0300

    Implemented getting of the current net usage and date&time.

--- bar.c
+++ bar.c
@@ -38,6 +38,36 @@
 
 #define MAX_LAYOUTS_COUNT 2
 
+/* Settings */
+#define INTERFACE "eth1"
+static char *tz = "Europe/Moscow";
+
+static Display *dpy;
+
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
+
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
+
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
+
+	return ret;
+}
+
 int
 getCurrentKeyboardLayout(Display *dpy, char *result)
 {    
@@ -139,6 +169,7 @@ getCurrentKeyboardLayout(Display *dpy, char *result)
     }    
     return 0;
 }
+
 void
 uppercase(char *str)
 {
@@ -148,21 +179,142 @@ uppercase(char *str)
         str[i] = toupper(str[i]);
     }
 }
+
+int
+parse_netdev(unsigned long long int *receivedabs, unsigned long long int *sentabs)
+{
+	char buf[255];
+	char *datastart;
+	static int bufsize;
+	int rval;
+	FILE *devfd;
+	unsigned long long int receivedacc, sentacc;
+
+	bufsize = 255;
+	devfd = fopen("/proc/net/dev", "r");
+	rval = 1;
+
+	// Ignore the first two lines of the file
+	fgets(buf, bufsize, devfd);
+	fgets(buf, bufsize, devfd);
+
+	while (fgets(buf, bufsize, devfd)) {
+	    if ((datastart = strstr(buf, INTERFACE)) != NULL) {
+		datastart = strstr(buf, ":");
+
+		// With thanks to the conky project at http://conky.sourceforge.net/
+		sscanf(datastart + 1, "%llu  %*d     %*d  %*d  %*d  %*d   %*d        %*d       %llu",\
+		       &receivedacc, &sentacc);
+		*receivedabs += receivedacc;
+		*sentabs += sentacc;
+		rval = 0;
+	    }
+	}
+
+	fclose(devfd);
+	return rval;
+}
+
+void
+calculate_speed(char *speedstr, unsigned long long int newval, unsigned long long int oldval)
+{
+	double speed;
+	speed = (newval - oldval) / 1024.0;
+	if (speed > 1024.0) {
+	    speed /= 1024.0;
+	    sprintf(speedstr, "%.3f MB/s", speed);
+	} else {
+	    sprintf(speedstr, "%.2f KB/s", speed);
+	}
+}
+
+char *
+get_netusage(unsigned long long int *rec, unsigned long long int *sent)
+{
+	unsigned long long int newrec, newsent;
+	newrec = newsent = 0;
+	char downspeedstr[15], upspeedstr[15];
+	static char retstr[42];
+	int retval;
+
+	retval = parse_netdev(&newrec, &newsent);
+	if (retval) {
+	    fprintf(stdout, "Error when parsing /proc/net/dev file.\n");
+	    exit(1);
+	}
+
+	calculate_speed(downspeedstr, newrec, *rec);
+	calculate_speed(upspeedstr, newsent, *sent);
+
+	sprintf(retstr, "D: %s U: %s", downspeedstr, upspeedstr);
+
+	*rec = newrec;
+	*sent = newsent;
+	return retstr;
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+void
+settz(char *tzname)
+{
+	setenv("TZ", tzname, 1);
+}
+char *
+mktimes(char *fmt, char *tzname)
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	bzero(buf, sizeof(buf));
+	settz(tzname);
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (timtm == NULL) {
+		perror("localtime");
+		exit(1);
+	}
+
+	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		exit(1);
+	}
+
+	return smprintf("%s", buf);
+}
+
 int
 main(void)
-{
-    static Display *dpy;
+{    
+    char *status;
     char layout[32];
+    char *netstats;
+    char *tm;
+    static unsigned long long int rec, sent;
+
+    parse_netdev(&rec, &sent);
     
     if (!(dpy = XOpenDisplay(NULL)))
     {
         fprintf(stderr, "dwmstatus: cannot open display.\n");
 	return 1;
     }
-    getCurrentKeyboardLayout(dpy, layout);
-    uppercase(layout);
-    printf("%s", layout);
-    
+    for (;;sleep(1)) {        
+        getCurrentKeyboardLayout(dpy, layout);
+        uppercase(layout);
+        netstats = get_netusage(&rec, &sent);
+        tm = mktimes("%a %x %X", tz);
+        status = smprintf("%s | %s | %s " ,
+                          layout, netstats, tm);
+        setstatus(status);        
+        free(status);
+    }    
     return 0;
 }
     
